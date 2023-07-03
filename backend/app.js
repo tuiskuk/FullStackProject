a52a78bff985815d03c506ac6a633ccb95813cb0
Santeri <santeri.ora@tuni.fi>
2023-07-03 03:04:22 +0300

    fixed recipe search to work with hard coded params

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
-const recipesRouter = require('./controllers/recipes')
 const express = require('express')
+const recipesRouter = require('./controllers/recipes')
+
 const app = express()
 
 app.use('/api/recipes', recipesRouter)
--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -1,42 +1,24 @@
 const config = require('../utils/config')
 const recipesRouter = require('express').Router()
-const axios = require('axios');
+const axios = require('axios')
 
-recipesRouter.get('/recipes', async (request, response) => {
-  const options = {
-    method: 'GET',
-    url: 'https://edamam-recipe-search.p.rapidapi.com/search',
-    params: {q: 'chicken'},
-    headers: {
-      'X-RapidAPI-Key': config.EDAMAM_APPLICATION_KEY,
-      'X-RapidAPI-Host': config.EDAMAM_ID,
-    }
-  }
-  
+recipesRouter.get('/', async (request, response) => {
   try {
-    const recipes = await axios.request(options)
-    console.log(recipes.data)
-    response.json(recipes.data)
+    const apiResponse = await axios.get(
+      'https://api.edamam.com/api/recipes/v2',
+      {
+        params: {
+          type: 'public',
+          q: 'pizza',
+          app_id: config.EDAMAM_ID,
+          app_key: config.EDAMAM_APPLICATION_KEY,
+        },
+      }
+    );
+    const recipes = apiResponse.data
+    response.json(recipes)
   } catch (error) {
-    console.error(error);
-  }
-
-  const optionss = {
-    method: 'GET',
-    url: 'https://edamam-recipe-search.p.rapidapi.com/search',
-    params: {q: 'chicken'},
-    headers: {
-      'X-RapidAPI-Key': config.EDAMAM_APPLICATION_KEY,
-      'X-RapidAPI-Host': config.EDAMAM_ID,
-    }
-  }
-  
-  try {
-    const recipes = await axios.request(optionss)
-    console.log(recipes.data)
-    response.json(recipes.data)
-  } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 })
 
