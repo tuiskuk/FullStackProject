3b71767234ae49187402a27ff93c898cf9292a8c
Santeri <santeri.ora@tuni.fi>
2023-07-04 15:52:31 +0300

    implemented filtering by diet/health

--- foodapp/src/services/recipes.js
+++ foodapp/src/services/recipes.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/recipes'
 
-const getAll = async (searchTerm) => {
-  const response = await axios.get(baseUrl, { params: { search: searchTerm } })
+const getAll = async (searchTerm, filterOptions) => {
+  const response = await axios.get(baseUrl, { params: { search: searchTerm, healthFilters: filterOptions } })
   return response.data
 }
 
