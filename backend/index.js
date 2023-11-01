48cea8723dfdc36c826f20980bb47ddcebd0588c
tuiskuk <tuisku.kaikuvuo2@gmail.com>
2023-11-01 16:17:09 +0200

    testing to tuisku branch

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js'
-import config from './utils/config.js'
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
-})
-
--- backend/routes/confirmEmailRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import confirmEmailController from '../controllers/confirmEmailController.js'
-const confirmEmailRouter = express.Router()
-
-
-confirmEmailRouter.get('/:emailToken', confirmEmailController.confirmEmail)
-
-export default confirmEmailRouter
